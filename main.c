9ba64c740dc36bc2aab82693532e449bd6ff82d8
Mido-Hyuga <elmehdibouslam@gmail.com>
2023-08-21 18:40:41 -0700

    shell cmd

---
+++ main.c
@@ -0,0 +1,65 @@
+#include "main.h"
+
+/**
+* free_lists - frees lists structure
+* @listssh: lists structure
+* Return: nothing
+*/
+void free_lists(lists_shell *listssh)
+{
+	unsigned int i;
+
+	for (i = 0; listssh->_environ[i]; i++)
+		free(listssh->_environ[i]);
+
+	free(listssh->_environ);
+	free(listssh->pid);
+}
+
+/**
+* set_lists - Initialize lists structure
+* @listssh: lists structure
+* @av: argument vector
+* Return: no return
+*/
+void set_lists(lists_shell *listssh, char **av)
+{
+	unsigned int i = 0;
+
+	listssh->av = av;
+	listssh->input = NULL;
+	listssh->args = NULL;
+	listssh->status = 0;
+	listssh->counter = 1;
+
+	while (environ[i])
+		i++;
+
+	listssh->_environ = malloc(sizeof(char *) * (i + 1));
+
+	for (i = 0; environ[i]; i++)
+		listssh->_environ[i] = _strdup(environ[i]);
+
+	listssh->_environ[i] = NULL;
+	listssh->pid = aux_itoa(getpid());
+}
+
+/**
+* main - Entry point
+* @ac: argument count
+* @av: argument vector
+* Return: 0.
+*/
+int main(int ac, char **av)
+{
+	lists_shell listssh;
+	(void) ac;
+
+	signal(SIGINT, get_sigint);
+	set_lists(&listssh, av);
+	shell_loop(&listssh);
+	free_lists(&listssh);
+	if (listssh.status < 0)
+		return (255);
+	return (listssh.status);
+}
